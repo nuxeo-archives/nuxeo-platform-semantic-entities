92aa5945aa732d5f2c375fffd9ce8e1ff408742f
Olivier Grisel <ogrisel@nuxeo.com>
2011-08-16 11:49:20 +0200

    NXSEM-27: fix test contrib filename change

--- nuxeo-platform-semantic-entities-core/src/test/java/org/nuxeo/ecm/platform/semanticentities/service/SemanticAnalysisServiceTest.java
+++ nuxeo-platform-semantic-entities-core/src/test/java/org/nuxeo/ecm/platform/semanticentities/service/SemanticAnalysisServiceTest.java
@@ -68,7 +68,7 @@ public class SemanticAnalysisServiceTest extends SQLRepositoryTestCase {
         // that needs an internet connection: comment the following contrib to
         // test again the real DBpedia server
         deployContrib("org.nuxeo.ecm.platform.semanticentities.core.tests",
-                "OSGI-INF/test-semantic-entities-remote-entity-contrib.xml");
+                "OSGI-INF/test-semantic-entities-dbpedia-entity-contrib.xml");
 
         // CMIS query maker
         deployBundle("org.nuxeo.ecm.core.opencmis.impl");
